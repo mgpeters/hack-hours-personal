8305483b788806e8a147bd7c040c509371a0475c
Matt Peters <matthew.george.peters@gmail.com>
2020-03-25 17:34:25 -0400

    cleaned up fxProgTest

--- challenges/functionalProgTest.js
+++ challenges/functionalProgTest.js
@@ -1,28 +1,15 @@
-
-const potion1 = '452A697B12';
-const potion2 = '567B125A';
-const potion3 = '2A4587B745';
-
-const mixedArray = [-1, 45, 100, 56, 7];
-
 const minMaxArray = (array) => {
   const sortedArray = array.sort((a, b) => a - b);
   return [sortedArray[0], sortedArray[array.length - 1]];
 };
 
-// console.log(minMaxArray(mixedArray));
-
 const addOne = (input) => {
   return input + 1;
 };
 
-// console.log(addOne(5));
-
 const subtractOne = (input) => {
   return input - 1;
 };
-
-// console.log(subtractOne(10));
 
 const mixedPotions = (str) => {
   const buildArray = [];
@@ -44,8 +31,6 @@ const mixedPotions = (str) => {
   return buildArray.join('').toString();
 };
 
-// console.log(mixedPotions(potion1));
-// console.log(mixedPotions(potion2));
-// console.log(mixedPotions(potion3));
-
-module.exports = { mixedPotions, addOne, subtractOne, minMaxArray }
+module.exports = {
+  mixedPotions, addOne, subtractOne, minMaxArray
+};
