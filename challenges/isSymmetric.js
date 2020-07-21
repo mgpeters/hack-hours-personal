80474aea237644ff8c949e9e18a43978b970aa17
Matt Peters <matthew.george.peters@gmail.com>
2020-07-21 14:28:42 -0400

    completed isSymmetric

--- challenges/isSymmetric.js
+++ challenges/isSymmetric.js
@@ -36,10 +36,60 @@
 
 // Recursive solution
 const isSymmetricRecursive = (root) => {
+  // recursive helper for mirror check
+  const isMirror = (tree1, tree2) => {
+    // Termination case: If both trees are null
+    if (tree1 === null && tree2 === null) return true;
+    // Termination case: If one tree is null
+    if (tree1 == null || tree2 === null) return false;
 
+    return (tree1.val === tree2.val)
+      && isMirror(tree1.right, tree2.left)
+      && isMirror(tree1.left, tree2.right);
+  }
+
+  return isMirror(root, root);
 };
 
 // Iterative solution
 const isSymmetricIterative = (root) => {
+  // Will confirm symmetry with an array/queue
+  const queue = [];
 
+  // will push both root's to queue off the bat
+  queue.push(root);
+  queue.push(root);
+
+  // loop through nodes
+  while (queue[0]) {
+    const t1 = queue.shift();
+    const t2 = queue.shift();
+
+    if (t1 === null && t2 === null) continue;
+    if (t1 === null || t2 === null) return false;
+    if (t1.val !== t2.val) return false;
+    queue.push(t1.left);
+    queue.push(t2.right);
+    queue.push(t1.right);
+    queue.push(t2.left);
+  }
+
+  return true;
 };
+
+function TreeNode(val, left, right) {
+  this.val = (val === undefined ? 0 : val)
+  this.left = (left === undefined ? null : left)
+  this.right = (right === undefined ? null : right)
+}
+
+const tree = new TreeNode(5);
+tree.left = new TreeNode(7);
+tree.right = new TreeNode(7);
+tree.right.right = new TreeNode(10);
+tree.left.left = new TreeNode(10);
+
+// console.log(isSymmetricRecursive(tree, tree));
+console.log(isSymmetricIterative(tree, tree));
+
+// console.log(Boolean([]));
