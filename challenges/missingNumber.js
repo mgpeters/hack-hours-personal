edc2f2b74b0d7ea351ad7b6b00a9a9975448cf91
Matt Peters <matthew.george.peters@gmail.com>
2020-08-13 09:54:57 -0400

    completed missinsNumber

--- challenges/missingNumber.js
+++ challenges/missingNumber.js
@@ -20,18 +20,33 @@
  * @return {number}
  */
 const missingNumber = (nums) => {
+  if (nums[0] === 0) return 1;
+
   let numsSum = 0;
   let actualSum = 0;
+  let lowest = Infinity;
+  let highest = -Infinity;
 
-  for (let i = 0; i < nums.length - 1; i += 1) {
+  for (let i = 0; i < nums.length; i += 1) {
+    if (nums[i] < lowest) lowest = nums[i];
+    if (nums[i] > highest) highest = nums[i];
     numsSum += nums[i];
+  }
+
+  // console.log(lowest)
+  // console.log(highest)
+
+  for (let i = lowest; i <= highest; i += 1) {
     actualSum += i;
   }
 
-  return numsSum - actualSum;
+  // console.log(numsSum);
+  // console.log(actualSum);
+
+  return actualSum - numsSum;
 };
 
 console.log(missingNumber([3, 0, 1])); // 2
 console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])); // 8
-console.log(missingNumber([0])); // 8
+console.log(missingNumber([0])); // 1
 
