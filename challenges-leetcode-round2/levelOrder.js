b3e14b741835709aa899770a5431196056a8d9ee
Matt Peters <matthew.george.peters@gmail.com>
2020-09-03 09:17:45 -0400

    completed maxDepth

--- challenges-leetcode-round2/levelOrder.js
+++ challenges-leetcode-round2/levelOrder.js
@@ -27,8 +27,20 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-const levelOrder = function (root) {
+const levelOrder = (root) => {
+  let maxDepth = 0;
+  // check if root is valid
+  if (!root) return maxDepth;
 
+  const bfsPreHelper = (leaf) => {
+    if (leaf === null) return 0; // check later
+
+    maxDepth = Math.max(bfsPreHelper(leaf.left), bfsPreHelper(leaf.right)) + 1;
+
+    return maxDepth;
+  };
+
+  return bfsPreHelper(root);
 };
 
 function TreeNode(val, left, right) {
@@ -44,7 +56,11 @@ tree.right = new TreeNode(8);
 
 tree.left.left = new TreeNode(2);
 tree.left.right = new TreeNode(4);
+// tree.left.right.right = new TreeNode(6);
+// tree.left.right.right.right = new TreeNode(6);
+
 
 tree.right.right = new TreeNode(10);
+tree.right.right.right = new TreeNode(15);
 
-console.log(levelOrder(tree));
+console.log(levelOrder(tree)); // 3
